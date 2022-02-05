9938ff5beef2d7f499bd6cefac7c72dab2130fa3
root <k359w3423@yahoo.co.jp>
2022-02-06 00:22:35 +0900

    色々やったあと

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { ProductDetailComponent } from './product-detail/product-detail.component';
+import { ProductListComponent } from './product-list/product-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "", component: ProductListComponent},
+  {path: "detail", component: ProductDetailComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
